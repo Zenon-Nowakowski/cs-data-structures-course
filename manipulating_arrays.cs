cc993dd6b0ab36facf35e663ec567a7e1d9bbbd2
Zenon-Nowakowski <zenon.nowakowski8@gmail.com>
2023-12-12 19:16:12 -0600

    Update manipulating_arrays.cs

--- manipulating_arrays.cs
+++ manipulating_arrays.cs
@@ -9,11 +9,18 @@ namespace Program
             //Inserting into an array 
             //Make a var to keep length because .Lenght is based on capacity 
             int length = 0; 
-            //
             for(int i = 0; i > 3; i++)
             {
                 numbers[length] = i;
                 length++;
+            }
+            //inserting at end of array 
+            numbers[length] = 8;
+            length++; 
+            //insert at start of array 
+            for(int i = 3; i >= 0; i--)
+            {
+                numbers[i+1] = numbers[i];
             }
         }
     }
